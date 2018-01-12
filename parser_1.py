9ba280ab85bb5846b54c0d3a03a0609c65478518
ZhekaZagoruiko <intuit07@gmail.com>
2018-01-12 21:42:50 +0200

    simpl skrit

---
+++ parser_1.py
@@ -0,0 +1,12 @@
+from google import google
+import sys
+
+word = sys.argv[1]
+s_res = google.search(word, 2)
+count = 0
+while count < 10:
+    count += 1
+    for el in s_res:
+        res = str(count)+'.'+str(el)
+    print(res)
+
